5e846f88d7ea34d57cb319eddb937d90e102a1c9
sameer <sameerxsub@gmail.com>
2026-05-27 20:01:18 +0530

    edit next config for aws

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  allowedDevOrigins: ["65.1.111.227"],
 };
 
 export default nextConfig;
